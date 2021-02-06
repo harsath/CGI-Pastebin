c1831f5ec6f8ffeb27599884f71b582ca334db1f
harsath <harsath@tuta.io>
2021-02-06 03:48:46 -0500

    Log support and syslog on die. eval on open()

--- bin.pl
+++ bin.pl
@@ -4,13 +4,16 @@ use strict;
 use constant BIN_DUMP_PATH => "/tmp/dump_bin";
 use constant SERVER_HOSTNAME => "cname.mydomain.tld";
 use constant CGI_LOCATION => "cgi-gateway";
+use constant LOG_DUMP_PATH => "/var/log/cgi_bin_log";
 use Digest::MD5 qw/md5_base64/;
 use File::Basename;
 
 my $request_method = $ENV{REQUEST_METHOD};
-my $bin_auth_header = "HTTP_X_BIN_AUTH";
-my $bin_auth_value = "SecurePassword";
 my $file_content_length = $ENV{CONTENT_LENGTH};
+my $client_ip = $ENV{REMOTE_ADDR};
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = gmtime(time);
+my $bin_auth_header = "HTTP_X_BIN_AUTH";
+my $bin_auth_value = "Secure Password";
 my @http_response_headers = (
         "Content-Type: text/plain"
 );
@@ -20,21 +23,17 @@ my @error_responses = <DATA>;
 my $http_id_error_msg = "";
 my $http_auth_error_msg = "";
 my $http_unknown_error_msg = "";
-
-open (LOGHANDLER, ">", "/usr/lib/cgi-gateway/log.txt") or system("logger Log ERROR $!");
+my $log_message = "$client_ip - [$hour:$min:$sec | $mday:$wday:$year] $request_method ";
 
 unless(-d BIN_DUMP_PATH){ mkdir(BIN_DUMP_PATH); }
 
 _populate_error_msg();
 
 if($request_method eq "GET"){
-        print LOGHANDLER "logger GET Request\n";
         http_get_processor();
 }elsif($request_method eq "POST"){
-        print LOGHANDLER "logger POST Request\n";
         http_post_processor();
 }else{
-        print LOGHANDLER "logger ERROR Request\n";
         http_error_handler("UNKNOWN");
 }
 
@@ -46,15 +45,18 @@ sub http_post_processor {
                 return;
         }
         if($ENV{$bin_auth_header} eq $bin_auth_value){
-                read(STDIN, my $file_content_data, $file_content_length);
-                my $content_digest = md5_base64($file_content_data);
-                open(FILEHANDLER, ">", BIN_DUMP_PATH."/".$content_digest) or die "filehandler error, dump";
-                print FILEHANDLER $file_content_data;
-                print LOGHANDLER "POST Data: $file_content_data\n";
-                close FILEHANDLER;
-                $http_response_body .= "https://".SERVER_HOSTNAME."/".CGI_LOCATION."/".$script_name."?id=".$content_digest."\n";
+		eval {
+			read(STDIN, my $file_content_data, $file_content_length);
+			my $content_digest = md5_base64($file_content_data);
+			open(FILEHANDLER, ">", BIN_DUMP_PATH."/".$content_digest) or die "$!";
+			print FILEHANDLER $file_content_data;
+			close FILEHANDLER;
+			$http_response_body .= "https://".SERVER_HOSTNAME."/".CGI_LOCATION."/".$script_name."?id=".$content_digest."\n";
+			$log_message .= " Successfully served POST ";
+		}; if($@){ system("logger POST FILEHANDLER die: $@"); die; }
         }else{
                 http_error_handler("AUTH");
+		$log_message .= " Post Invalid Auth Error ";
         }
 }
 
@@ -64,25 +66,28 @@ sub http_get_processor {
                 return;
         }
         my ($query_key, $query_value) = split("=", $ENV{QUERY_STRING}) or die("split error");
-        print LOGHANDLER "\nID: $query_key";
+	$log_message .= " ID: $query_key";
         my $file_path = BIN_DUMP_PATH."/".$query_value;
         if($query_key eq "id"){
                 if(-e $file_path){
-                        open(FILEHANDLER, "<", $file_path) or die "filehandler error, get";
-                        my @read_file_contentsRef = <FILEHANDLER>;
-                        my $read_file_contents = "";
-                        for (@read_file_contentsRef){
-                                $read_file_contents .= $_;
-                        }
-                        $http_response_body .= $read_file_contents;
-                        close FILEHANDLER;
+			eval {
+				open(FILEHANDLER, "<", $file_path) or die "filehandler error, get";
+				my @read_file_contentsRef = <FILEHANDLER>;
+				my $read_file_contents = "";
+				for (@read_file_contentsRef){
+					$read_file_contents .= $_;
+				}
+				$http_response_body .= $read_file_contents;
+				close FILEHANDLER;
+				$log_message .= " Successfully served GET ";
+			}; if($@){ system("logger GET FILEHANDLER error: $@"); die; }
                 }else{
-                        print LOGHANDLER "logger Error Log";
                         http_error_handler("ID");
+			$log_message .= " File not exist ";
                 }
         }else{
-                print LOGHANDLER "logger Error Log";
                 http_error_handler("ID");
+		$log_message .= " Invalid query key ";
         }
 }
 
@@ -103,13 +108,13 @@ sub http_responder {
         for (@http_response_headers){
                 $http_message .= "$_\n\r";
         }
-        print LOGHANDLER "logger http-message: $http_message";
         $http_message .= "\n\r$http_response_body";
-        print LOGHANDLER "logger http-message: $http_message";
-        print LOGHANDLER "Message: $http_message";
-        print LOGHANDLER "HTTP Request method: $request_method\n";
-        print LOGHANDLER "Content-Legth: $file_content_length";
         print $http_message;
+	eval {
+		open (LOGHANDLER, ">>", LOG_DUMP_PATH) or die "$!";
+		print LOGHANDLER $log_message."\n";
+		close LOGHANDLER;
+	}; if($@){ system("logger LOGHANDLER: $@"); die; }
 }
 
 sub _populate_error_msg {
